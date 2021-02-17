0078ac6c25d713745932687ff4a588076a0998e6
maxim <max280596@gmail.com>
2021-02-17 10:30:28 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form class="form" action="">
+  <fieldset className="field">
+    <label for="default-text">Имя </label>
+    <input type="text" id="default-text" className="text" />
+  </fieldset>
+  <fieldset className="field">
+    <label for="default-text">Фамилия </label>
+    <input type="text" id="default-text" className="text" />
+  </fieldset>
+  <fieldset className="field">
+    <label for="default-text">Email </label>
+    <input type="email" id="default-text" className="email" />
+  </fieldset>
+  <fieldset className="field">
+    <label for="default-text">Пароль </label>
+    <input type="password" id="default-text" className="pass" />
+  </fieldset>
+
+  <fieldset className="field submit">
+    <input type="submit" value="Send" />
+    <input type="reset" value="Clear" />
+  </fieldset>
+</form>
+
     </div>
   );
 }
