66705420ccdf05648d164754c06f62cc17c5405d
maxim <max280596@gmail.com>
2021-02-17 12:55:04 +0200

    form 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,76 @@
 
 import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div `
+height: 40em;
+    position: relative
+`
+
+const Button = styled.button `
+color: palevioletred;
+  font-size: 20px;
+  margin: 10px;
+  padding: 5px 20px;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+
+const Form = styled.form `
+margin: 0;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)
+
+`
+const Input = styled.input `
+color: palevioletred;
+  font-size: 20px;
+  margin: 10px;
+  padding: 5px 20px;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  width: 100%;
+  box-sizing: border-box;
+`
+
+const Div = styled.div `
+
+`
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-       <form class="form" action="">
-  <fieldset className="field">
-    <label for="default-text">Имя </label>
-    <input type="text" id="default-text" className="text" />
-  </fieldset>
-  <fieldset className="field">
-    <label for="default-text">Фамилия </label>
-    <input type="text" id="default-text" className="text" />
-  </fieldset>
-  <fieldset className="field">
-    <label for="default-text">Email </label>
-    <input type="email" id="default-text" className="email" />
-  </fieldset>
-  <fieldset className="field">
-    <label for="default-text">Пароль </label>
-    <input type="password" id="default-text" className="pass" />
-  </fieldset>
+    <Container className="App">
 
-  <fieldset className="field submit">
-    <input type="submit" value="Send" />
-    <input type="reset" value="Clear" />
-  </fieldset>
-</form>
+      <Form action= "/" method = "post">
+       <Div className="username">
 
-    </div>
+      <Input type="text" id="name" name="user_name" placeholder="Name"></Input>
+      </Div>
+      <Div className="userSurname">
+      <Input type="text" id="name" name="user_name" placeholder="Surname"></Input>
+      </Div>
+    <Div className="usermail">
+      <Input type="email" id="mail" name="user_mail" placeholder="Email"></Input>
+      </Div>
+    <Div className = "userpassword">
+      <Input type="password" id="pass" name="user_pass" placeholder="Password"></Input>
+      </Div>
+    <div className="button">
+    <Button type="reset">reset</Button>
+  <Button type="submit">Send</Button>
+</div>
+
+  </Form>
+
+    </Container>
   );
 }
 
